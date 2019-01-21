59b1390b786630a01af035081db8aab5e42e3d55
Nicolas <nicolasduret@outlook.com>
2019-01-21 13:52:59 +0100

    salut c'est la resta des bacs à sable

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ session_start();
 //include du fichier CONTROLEUR
 $path=$_SERVER['DOCUMENT_ROOT'].'/PPE_InfraWeb';
 include 'Controleur.php';
-//SI le controleur n'existe pas d�j�, on l'instancie
+//SI le controleur n'existe pas déjà, on l'instancie
 if (!isset ($_SESSION['Controleur']))
 	{
 	$monControleur = new Controleur();
@@ -17,7 +17,7 @@ else
 $monControleur->afficheEntete();
 //affichage du menu
 $monControleur->afficheMenu();
-//SI on souhaite voir un vue particuli�re, on passe celle-ci en param�tre (elle est r�cup�r�e � travers la m�thode GET)
+//SI on souhaite voir un vue particulière, on passe celle-ci en param�tre (elle est récupérée à travers la méthode GET)
 if ((isset($_GET['vue']))&& (isset($_GET['action'])))
 	$monControleur->affichePage($_GET['action'],$_GET['vue']);
 //affichage du pied de page
