002626ca92ba08de2abb2bdd8e988b571afac0d7
Nicolas <nicolasduret@outlook.com>
2019-01-25 10:55:01 +0100

    le push de dieux

---
+++ Vues/diapo.php
@@ -0,0 +1,34 @@
+<div class="diapo">
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="Images/arc.jpg" class="d-block" alt="">
+
+            </div>
+            <div class="carousel-item">
+                <img src="Images/basket.jpg" class="d-block" alt="">
+
+            </div>
+            <div class="carousel-item">
+                <img src="Images/natation.jpg" class="d-block" alt="">
+
+            </div>
+            <div class="carousel-item">
+                <img src="Images/rugby.jpg" class="d-block" alt="">
+
+            </div>
+            <div class="carousel-item">
+                <img src="Images/judo.jpg" class="d-block" alt="">
+
+            </div>
+            <div class="carousel-item">
+                <img src="Images/gym.jpg" class="d-block" alt="">
+
+            </div>
+            <div class="carousel-item">
+                <img src="Images/hand.jpg" class="d-block" alt="">
+
+            </div>
+        </div>
+    </div>
+</div>
