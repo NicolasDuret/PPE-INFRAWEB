9e85bb0a25bee38622af07e11ba2452051d2133c
leo <leo.catifait@epsi.fr>
2019-03-06 10:32:16 +0100

    multiplication de tableau resolu

--- index.php
+++ index.php
@@ -19,14 +19,7 @@ $monControleur->afficheEntete();
 //affichage du menu
 $monControleur->afficheMenu();
 //SI on souhaite voir un vue particulière, on passe celle-ci en param�tre (elle est récupérée à travers la méthode GET)
-if ((isset($_GET['vue']))&& (isset($_GET['action']))){echo "<script>$('#carouselExampleIndicators').removeClass('d-block').addClass('d-none');
-</script>";$monControleur->affichePage($_GET['action'],$_GET['vue']);
-}
-else {echo "<script>$('#carouselExampleIndicators').removeClass('d-none').addClass('d-block');
-</script>";}
 
-//affichage du pied de page
-$monControleur->affichePiedPage();
 
 if ((isset($_GET['vue']))&& (isset($_GET['action']))){
     echo "<script>$('#carouselExampleIndicators').removeClass('d-block').addClass('d-none');</script>";
